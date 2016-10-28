b6eccce97ee75adeb88c32ce4cad85e2abda7543
ashish <ashish@localhost.localdomain>
2016-10-27 22:21:54 -0700

    Adds nodeMcu sample.

---
+++ nodeMcu/src/test.ino
@@ -0,0 +1,24 @@
+#include "Arduino.h"
+
+#define LED_PIN    2
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_PIN, HIGH);
+
+  // wait for a second
+  delay(1000);
+
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_PIN, LOW);
+
+   // wait for a second
+  delay(1000);
+}
