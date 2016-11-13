8062dbab29c3b5b20e9286de3cd605deb84f9e35
ashish <ashish@localhost.localdomain>
2016-11-13 12:07:43 -0800

    Add a recursive build system
    Adds recursive build system structure based on pmake and pbuild scripts.

--- scripts/pmake.py
+++ scripts/pmake.py
@@ -1,7 +1,31 @@
-#/usr/bin/python
+import argparse
+import sys, os
+from subprocess import call
 
-print("--------------------------------------")
-print("--------------------------------------")
-print("Invoking pluto build")
-print("--------------------------------------")
-print("--------------------------------------")
+print "\n"
+print "----------------------------------------"
+print "----------------------------------------"
+print "----------------------------------------"
+print "------Invoking Pluto Build System-------"
+print "----------------------------------------"
+print "----------------------------------------"
+print "----------------------------------------"
+print "\n"
+
+
+#----------------------------------------
+# Print current directory
+#----------------------------------------
+print "Current dir:",os.getcwd()
+print "\n"
+
+
+#----------------------------------------
+# Check arguments
+#----------------------------------------
+if len(sys.argv) > 1:
+    if sys.argv[1] == "clean":
+        call(["python","pbuild","clean"])
+
+if len(sys.argv) == 1:
+    call(["python","pbuild"])
