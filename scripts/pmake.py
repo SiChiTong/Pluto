1ae84056f992f8a3741d7230701bb5661897b9c7
ashish choudhari <ashish@seriforge.com>
2017-01-18 17:33:34 -0800

    Build fix

--- scripts/pmake.py
+++ scripts/pmake.py
@@ -20,12 +20,44 @@ print "Current dir:",os.getcwd()
 print "\n"
 
 
+# Check if dir exist, else create new one
+def makedir(p):
+    if not os.path.exists(p):
+        os.makedirs(p)
+
+# Check if dir exist and delete it
+def rmdir(p):
+    if os.path.exists(p):
+        os.rmdir(p)
+
 #----------------------------------------
 # Check arguments
 #----------------------------------------
 if len(sys.argv) > 1:
+    proot = os.environ.get('PLUTO_ROOT')
+
+    # Clean
     if sys.argv[1] == "clean":
         call(["python","pbuild","clean"])
+        if None == proot:
+            print "PLUTO_ROOT not set"
+        else:
+            rmdir(proot+'/deploy/'+'bin')
+            rmdir(proot+'/deploy/'+'labs')
+            rmdir(proot+'/deploy')
+
+    # Deploy
+    if sys.argv[1] == "deploy":
+        # Create release filesystem
+
+        if None == proot:
+            print "PLUTO_ROOT not set"
+        else:
+            makedir(proot+'/deploy/'+'bin')
+            makedir(proot+'/deploy/'+'labs')
+            call(["python","pbuild","deploy"])
 
 if len(sys.argv) == 1:
+
+    # Build
     call(["python","pbuild"])
